3ed9f6409d987f22d9290c5945de547b857e9484
Jessica Sampaio <jessicamsampaio@gmail.com>
2024-03-10 19:25:42 -0300

    commit de segurança

---
+++ src/scripts/main.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
+            aba.classList.add('premiacoes__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('premiacoes__tabs__button--is-active');
+        })
+    }
+})
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('premiacoes__tabs__button--is-active');
+    }
+}
+
+function escondeTodasAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('premiacoes__list--is-active');
+    }
+}
